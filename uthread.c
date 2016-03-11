55d18edfcfd8af4f080591e0f89995898ad40d9c
Dennis Chen <dchen043@ucr.edu>
2016-03-10 20:13:25 -0800

    Finished implementing locks

--- uthread.c
+++ uthread.c
@@ -9,14 +9,17 @@ int thread_create(void *(*start_routine)(void*), void *arg){
 }
 
 
-void lock_init(lock_t *mlock){
-  //TODO
+void lock_init(lock_t *lck){
+  *lck=0;
 }
-void lock_acquire(lock_t * mlock){
-  //TODO
+void lock_acquire(lock_t *lck){
+  while(xchg(mlock,1)!=0)//already 1, held
+    ;//spin
+  return;//received lock
 }
 
-void lock_release(lock_t * mlock){
-//TODO
+void lock_release(lock_t * lck){
+  *mlock = 0;
+  return;
 }
 
